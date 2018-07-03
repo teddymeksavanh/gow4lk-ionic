2a28bf16fa1b36a1b98298880dad4c2c5b70ed05
teddymeksavanh <teddymeksavanh@yahoo.fr>
2018-07-03 11:03:04 +0200

    Login and Signin are up

--- gow4lk/src/providers/api/api.ts
+++ gow4lk/src/providers/api/api.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
  */
 @Injectable()
 export class Api {
-  url: string = 'https://example.com/api/v1';
+  url: string = 'http://0.0.0.0:3000';
 
   constructor(public http: HttpClient) {
   }
