8fb6fc53fe279723af3afdd35395b998dcb7dd37
teddymeksavanh <ventialaw@hotmail.fr>
2018-07-14 01:03:31 +0200

    Search input

--- gow4lk/src/pages/path-create/path-create.ts
+++ gow4lk/src/pages/path-create/path-create.ts
@@ -156,43 +156,61 @@ export class PathCreatePage {
           scrollwheel: false
         });
 
-        if (this.readonly === false) {
-          
-          // const drawingManager = new google.maps.drawing.DrawingManager({
-          //   drawingMode: null,
-          //   drawingControl: true,
-          //   drawingControlOptions: {
-          //     position: google.maps.ControlPosition.BOTTOM_CENTER,
-          //     drawingModes: ['polyline']
-          //   },
-          //   polylineOptions: this.commonPolylineConfig
-          // });
-          // drawingManager.setMap(map);
+        console.log('google.maps', google.maps);
 
-          // google.maps.event.addListener(
-          //   drawingManager,
-          //   'overlaycomplete',
-          //   event => {
-          //     const polyline = event.overlay;
-          //     if (event.type == 'polyline') {
-          //       if (drawingManager) {
-          //         drawingManager.setDrawingMode(null);
-          //       }
-          //       this.attachClickEventOnPolyline(polyline);
+        // More
 
-          //       this.attachInsertAtEventOnPolyline(polyline);
+        let input = document.getElementById('pac-input');
+        let searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-          //       this.attachSetAtEventOnPolyline(polyline);
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
 
-          //       this.addPolylineToCollection({polyline: polyline});
-          //       this.selectShape(polyline, event);
-          //       if (this.emitOnOverlayComplete) {
-          //         this.fromMapPathsToDB();
-          //       }
-          //     }
-          //   }
-          // );
-        }
+        var markers = [];
+        
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+
+          map.fitBounds(bounds);
+        });
+
+        // Fin more
+
         resolve(map);
       });
     });
